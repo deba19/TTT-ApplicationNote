7fc1e6190fa237336ea53263d1071417dc5a9d5c
Debasis Jana <debasisjana93@gmail.com>
2020-08-03 20:54:55 +0530

    Basic setup done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs=require('fs')
+const http=require('http')
+const express=require('express')
+const app = express()
+const path= require('path')
+const server=http.createServer(app)
+const port = process.env.PORT || 3000
+const publicDirectoryPath= path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+server.listen(port,()=>
+{
+    console.log('Server is up on ',port)
+})
