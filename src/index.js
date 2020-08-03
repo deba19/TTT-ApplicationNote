9053f34e0e0605a69aad6634298fa8f8403f87cd
Debasis Jana <debasisjana93@gmail.com>
2020-08-03 21:15:28 +0530

    Text file fetched using http request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const fs=require('fs')
+const request=require('request')
 const http=require('http')
 const express=require('express')
 const app = express()
@@ -7,6 +7,17 @@ const server=http.createServer(app)
 const port = process.env.PORT || 3000
 const publicDirectoryPath= path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
+
+//Http call to read the text file from the given URL
+request.get('https://terriblytinytales.com/test.txt','utf8',(err,res,data)=>{
+    if(err || res.statusCode!==200)
+    {
+        console.log("Give URL is invalid")
+    }
+    data=data
+    
+    //console.log(data)
+})
 
 server.listen(port,()=>
 {
